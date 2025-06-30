9db51f1f3a697145aa2d4025560343fc82d52877
David Borrego Valenzuela <davidborregomdl@gmail.com>
2025-06-30 12:02:23 +0200

    Remove test directory

--- test/safe_code.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def vulnerable_function():
-    user_input = input("Enter your name: ") 
-    if not user_input.isalnum():  # Simple validation: only alphanumeric characters
-        return
-    os.system("echo " + user_input)         
-
-vulnerable_function()
--- test/vulnerable_code.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def vulnerable_function():
-    name = input("Enter your name: ")
-    debug = name.lower()
-    test = debug
-    os.system(test)
-
-vulnerable_function()
